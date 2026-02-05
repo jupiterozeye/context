78f63a6ffdae025ad7869b9f09e598da1601b30a
jupiterozeye <ozeye@tutamail.com>
2026-02-05 11:45:27 +0100

    new hash

--- flake.nix
+++ flake.nix
@@ -6,57 +6,60 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        version = "0.1.0";
-      in
-      {
-        packages = {
-          context = pkgs.buildGoModule {
-            pname = "context";
-            inherit version;
-            src = ./.;
-            
-            vendorHash = "sha256-OxV4XCXVQJiA4VJNlncP5gehfOsfSnqVAIPZWRXd9AM=";
-            
-            ldflags = [
-              "-s"
-              "-w"
-              "-X main.version=${version}"
-            ];
-            
-            buildInputs = with pkgs; [ wl-clipboard ];
-            
-            postInstall = ''
-              mkdir -p $out/share/context/shell
-              cp -r $src/shell/* $out/share/context/shell/
-              
-              wrapProgram $out/bin/context \
-                --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [ wl-clipboard xclip ])}
-            '';
-            
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-            
-            meta = with pkgs.lib; {
-              description = "Terminal context capture tool for AI-assisted debugging";
-              homepage = "https://github.com/jupiterozeye/context";
-              license = licenses.mit;
-              maintainers = [ ];
-              platforms = platforms.all;
-            };
-          };
-          default = self.packages.${system}.context;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      version = "0.1.0";
+    in {
+      packages = {
+        context = pkgs.buildGoModule {
+          pname = "context";
+          inherit version;
+          src = ./.;
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go
-            gopls
-            gofumpt
-            golangci-lint
+          vendorHash = "sha256-uJ2QwlRmyocxCEgoDT15giDDr6GkveTGdmdPTAZVW2w=";
+
+          ldflags = [
+            "-s"
+            "-w"
+            "-X main.version=${version}"
           ];
+
+          buildInputs = with pkgs; [wl-clipboard];
+
+          postInstall = ''
+            mkdir -p $out/share/context/shell
+            cp -r $src/shell/* $out/share/context/shell/
+
+            wrapProgram $out/bin/context \
+              --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [wl-clipboard xclip])}
+          '';
+
+          nativeBuildInputs = [pkgs.makeWrapper];
+
+          meta = with pkgs.lib; {
+            description = "Terminal context capture tool for AI-assisted debugging";
+            homepage = "https://github.com/jupiterozeye/context";
+            license = licenses.mit;
+            maintainers = [];
+            platforms = platforms.all;
+          };
         };
-      });
+        default = self.packages.${system}.context;
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          gopls
+          gofumpt
+          golangci-lint
+        ];
+      };
+    });
 }
+
